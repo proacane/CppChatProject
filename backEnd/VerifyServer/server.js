0f528e5047bc55e728be3f6b1af78df6592f483e
ACANE <ztmy19598@gmail.com>
2024-08-16 14:27:21 +0800

    nodejs实现邮箱验证

---
+++ backEnd/VerifyServer/server.js
@@ -0,0 +1,41 @@
+const grpc = require('@grpc/grpc-js')
+const message_proto = require('./proto')
+const const_module = require('./const')
+const { v4: uuidv4 } = require('uuid');
+const emailModule = require('./email');
+
+
+async function GetVerifyCode(call, callback) {
+    console.log("email is ", call.request.email)
+    try{
+        uniqueId = uuidv4();
+        console.log("uniqueId is ", uniqueId)
+        let text_str =  '您的验证码为\n'+ uniqueId +'\n请三分钟内完成注册'
+        
+        let mailOptions = {
+            from: 'ztmy19598@163.com',
+            to: call.request.email,
+            subject: '验证码',
+            text: text_str,
+        };
+        let send_res = await emailModule.SendMail(mailOptions);
+        console.log("send res is ", send_res)
+        callback(null, { email:  call.request.email,
+            error:const_module.Errors.Success
+        }); 
+    }catch(error){
+        console.log("catch error is ", error)
+        callback(null, { email:  call.request.email,
+            error:const_module.Errors.Exception
+        }); 
+    }
+}
+function main() {
+    var server = new grpc.Server()
+    server.addService(message_proto.VerifyService.service, { GetVerifyCode: GetVerifyCode })
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+        // server.start()
+        console.log('grpc server started')        
+    })
+}
+main()
